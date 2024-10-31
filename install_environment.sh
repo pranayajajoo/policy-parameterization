ddd5cd9291fd32fd1e3c1eb4169cf221c48cb4df
hejiamin <hejm37@gmail.com>
2024-10-30 20:41:20 -0600

    fix: bug in bash script

--- install_environment.sh
+++ install_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 user_id="jiamin"
-pi_id="def-whitem"
 
+module load StdEnv/2020
 module load python/3.9.6 mujoco
 virtualenv --no-download ~/policy_params
 
@@ -13,7 +13,6 @@ tar -zvxf ~/Downloads/mujoco210-linux-x86_64.tar.gz -C ~/Downloads/
 export MUJOCO_PY_MUJOCO_PATH=/home/$user_id/Downloads/mujoco210
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/$user_id/Downloads/mujoco210/bin:/usr/lib/nvidia
 
-cd ~/project/$pi_id/$user_id/policy-params
 pip install --no-index --upgrade pip
 pip install -r requirements.txt
 pip install gym==0.21.0 dm_control==1.0.11 mujoco-py==2.1.2.14
